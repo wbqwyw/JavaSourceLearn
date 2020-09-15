6038afbca4fab0e3d9a92240c251be6a3a40d416
wangbq3 <yonyou2016%>
2020-09-15 13:45:43 +0800

    Initial commit

---
+++ src/com/wangbq/test/hashmap/HashMapTest.java
@@ -0,0 +1,20 @@
+package com.wangbq.test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName HashMapTest
+ * @Description hashMap调试类
+ * @Author Administrator
+ * @Date 2020/9/11 13:57
+ * @Version 1.0
+ */
+public class HashMapTest {
+    public static void main(String[] args) {
+        Map<String, Double> map = new HashMap<>();
+        map.put("Mk1", 1.1);
+        map.put("Mk2", 1.2);
+        map.put("Mk3", 1.3);
+    }
+}
